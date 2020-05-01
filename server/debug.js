b7daaad0df4e49905490f898978c6969f3d61497
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-05-01 13:26:39 +0100

    logging and dist ignore

--- server/debug.js
+++ server/debug.js
@@ -1,6 +1,6 @@
 const DEBUG = process.env.DEBUG_LOG === "true" || process.env.NODE_ENV !== "production";
 
-exports.log = console.log;
+exports.log = (msg) => console.log(`[Pulse]: ${msg}`);
 
 exports.debugLog = (msg) => {
   if (DEBUG) console.log(`[Pulse]: ${msg}`);
--- src/debug.js
+++ src/debug.js
@@ -1,7 +1,7 @@
 const DEBUG = process.env.DEBUG_LOG === "true" || process.env.NODE_ENV !== "production";
 
-export const log = console.log;
+export const log = (msg) => console.log(`[Pulse Client]: ${msg}`);
 
 export const debugLog = (msg) => {
-  if (DEBUG) console.log(msg);
+  if (DEBUG) console.log(`[Pulse Client]: ${msg}`);
 };
