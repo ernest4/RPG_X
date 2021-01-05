aa8fb9154c67263675c0244b2994bce75e370a8a
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2021-01-05 07:41:37 +0000

    jest set up

--- src/ecs/tests/unit/Vector3DataView.test.ts
+++ src/ecs/tests/unit/Vector3DataView.test.ts
@@ -1,13 +1,21 @@
 // const { testAdder } = require("../src/testy2");
 
-// test("adds 1 + 2 to equal 3", () => {
+import { context } from "../../../../tests/testAliases";
+
+// it("adds 1 + 2 to equal 3", () => {
 //   expect(1 + 2).toBe(3);
 // });
 
-// test("adds numbers", () => {
+// it("adds numbers", () => {
 //   expect(testAdder(1, 2)).toBe(3);
 // });
 
-
 // TODO: tests, start doing them !!
 
+describe("Vector3DataView", () => {
+  context("123", () => {
+    it("works", () => {
+      expect(3 + 2).toEqual(5);
+    });
+  });
+});
--- tests/my.test.ts
+++ tests/my.test.ts
@@ -7,3 +7,7 @@
 // test("adds numbers", () => {
 //   expect(testAdder(1, 2)).toBe(3);
 // });
+
+it("works", () => {
+  expect(1 + 3).toEqual(4);
+});
